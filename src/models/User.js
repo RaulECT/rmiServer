1e81b6b15468d6f575b772331c1c073ca956c154
Raul <eduardoct120@gmail.com>
2017-04-17 16:03:25 -0500

    User sign in implemented

---
+++ src/models/User.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const mongoose = require( 'mongoose' )
+
+const Schema = mongoose.Schema
+const UserSchema = new Schema( {
+  email: { type: String, unique: true, lowercase: true },
+  password: String
+} )
+
+module.exports = mongoose.model( 'User', UserSchema )
